7cd8d745274fd66f96b1059cf023ae9444d9b835
MiguelBits <mcv.cooper@gmail.com>
2026-01-29 01:46:32 +0000

    Add CORS support for cross-origin requests

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,13 +2,25 @@ package router
 
 import (
 	"algebra-apr-backend/internal/handlers"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func SetupRouter(db *gorm.DB) *gin.Engine {
 	r := gin.Default()
+
+	// Configure CORS to allow all origins
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	handler := handlers.NewHandler(db)
 
